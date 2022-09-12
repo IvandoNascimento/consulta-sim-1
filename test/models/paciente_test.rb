8bff9b3611ff3a8bc1386f7a2d94575caa3a35ce
Ivan do Nascimento <nascimentodoivan@gmail.com>
2022-09-12 02:28:14 -0300

    Implementação dos testes de Paciente

--- test/models/paciente_test.rb
+++ test/models/paciente_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class PacienteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'não pode criar paciente sem email ' do
+    paciente = Paciente.new nome: 'Rodrigo Andrade',data_nascimento: '04-03-2001',
+                            cpf: '161169850-25', email: ''
+    assert_not paciente.save
+
+  end
+
+  test 'não pode criar paciente sem nome' do
+    paciente = Paciente.new nome: '',data_nascimento: '04/03/2001', cpf: '161169850-25', email: 'rodrigo@gmail.com'
+    assert_not paciente.save
+  end
 end
