2c2e528d54e4cc9fd21056506174602aeb803bf0
Ivan do Nascimento <nascimentodoivan@gmail.com>
2022-09-12 02:28:31 -0300

    Implementação dos testes de Medico

--- test/models/medico_test.rb
+++ test/models/medico_test.rb
@@ -1,9 +1,15 @@
 require "test_helper"
 
 class MedicoTest < ActiveSupport::TestCase
-  test 'criando medico corretamente' do
-    medico = Medico.new nome: 'Rodrigo Andrade', cpf: '706.508.980-01', email: 'rodrigo@rodrigo.com',
+  test 'não pode criar medico sem email ' do
+    medico = Medico.new nome: 'Rodrigo Andrade', cpf: '161169850-25', email: '',
                         especialidade: 'Reumatologista', crm: '123456'
-    assert medico.save
+    assert_not medico.save
+  end
+  test 'não pode criar medico sem nome' do
+    medico = Medico.new nome: '', cpf: '161169850-25', email: 'rodrigo@gmail.com',
+                        especialidade: 'Reumatologista', crm: '123456'
+    assert_not medico.save
   end
 end
+
