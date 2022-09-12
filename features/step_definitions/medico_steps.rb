10d1c1caed818163b7a59af501c4865a5d3164bc
Ivan do Nascimento <nascimentodoivan@gmail.com>
2022-09-12 02:36:49 -0300

    Implementado os passos pro primeiro cenario de Medico

--- features/step_definitions/medico_steps.rb
+++ features/step_definitions/medico_steps.rb
@@ -1,38 +1,18 @@
 Given('estou na pagina de medico') do
-  visit 'medicos/new'
+  visit '/medicos/new'
   expect(page).to have_content('New medico')
 end
 
-Given('o medico com CRM {string} existe') do |crm|
-  fill_in 'medico[nome]', :with => 'Rodrigo Andrade'
-  fill_in 'medico[cpf]', :with => '706.508.980-01'
-  fill_in 'medico[email]', :with => 'rodrigo@rodrigo.com'
-  fill_in 'medico[especialidade]', :with => 'Reumatologista'
+When('eu preencho um medico de nome: {string}, cpf: {string}, email: {string}, especialidade: {string}, crm: {string} e clico cadastrar') do |nome, cpf, email, especialidade,crm|
+  fill_in 'medico[nome]', :with => nome
+  fill_in 'medico[cpf]', :with => cpf
+  fill_in 'medico[email]', :with => email
+  fill_in 'medico[especialidade]', :with => especialidade
   fill_in 'medico[crm]', :with => crm
   click_button 'Create Medico'
-  expect(page).to have_content(crm)
 end
 
-When('eu clico em remover o medico com CRM {string}') do |crm|
-  click_button 'Destroy this medico'
+Then('eu vejo uma mensagem de erro de nome invalido') do
+  assert_selector('li', text: 'Nome is too short (minimum is 5 characters)')
 end
 
-Then('eu vejo uma mensagem que o medico foi apagado com sucesso') do
-  expect(page).to have_content('Medico was successfully destroyed')
-end
-
-When('eu preencho os campos de nome {string}, cpf {string}, email {string}, especialidade {string} e crm {string}') do |string, string2, string3, string4, string5|
-  fill_in 'medico[nome]', :with => string
-  fill_in 'medico[cpf]', :with => string2
-  fill_in 'medico[email]', :with => string3
-  fill_in 'medico[especialidade]', :with => string4
-  fill_in 'medico[crm]', :with => string5
-end
-
-When('eu clico em cadastrar novo medico') do
-  click_button 'Create Medico'
-end
-
-Then('eu vejo uma mensagem que o medico foi cadastrado com sucesso') do
-  expect(page).to have_content('Medico was successfully created.')
-end
