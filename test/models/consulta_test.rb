195429884d0b537cb66dc73db0bd9467841ddff1
Ivan do Nascimento <nascimentodoivan@gmail.com>
2022-09-12 02:24:45 -0300

    Implementação dos testes de Consulta

--- test/models/consulta_test.rb
+++ test/models/consulta_test.rb
@@ -1,7 +1,29 @@
 require "test_helper"
 
 class ConsultaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "deve salvar consulta corretamente" do
+    paciente = Paciente.new nome: 'Ivan teste', data_nascimento: '04/03/2001', cpf: '161169850-25', email: 'teste@teste.com'
+
+    assert paciente.save
+
+    medico = Medico.new nome: 'Rodrigo Andrade', cpf: '161169850-25', email: 'rodrigo@rodrigo.com',
+                        especialidade: 'Reumatologista', crm: '123456'
+    assert medico.save
+
+    consulta = Consulta.new data: '20/12/2022', horario: '11:00', medico_id: medico.id, paciente_id: paciente.id
+    assert consulta.save()
+  end
+  test 'não pode criar consulta sem medico' do
+
+    paciente = Paciente.new nome: 'Ivan teste', data_nascimento: '04-03-2001', cpf: '161169850-25', email: 'teste@teste.com'
+    assert paciente.save
+
+
+    medico = Medico.new nome: 'Rodrigo Andrade', cpf: '161169850-25', email: 'rodrigo@rodrigo.com',
+                        especialidade: 'Reumatologista', crm: ''
+    assert_not medico.save
+
+    consulta = Consulta.new(data: '20-12-2022', horario: '11:00', medico_id: medico.id, paciente_id: paciente.id)
+    assert_not consulta.save()
+  end
 end
